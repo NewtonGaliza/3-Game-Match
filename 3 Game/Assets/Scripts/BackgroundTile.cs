9beae5c53d34c8415efd663c46c85874b48613bc
Newton Galiza <newtonjgaliza@gmail.com>
2022-11-11 17:54:59 -0300

    First Commit
    First Commit

---
+++ 3 Game/Assets/Scripts/BackgroundTile.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundTile : MonoBehaviour
+{
+    public GameObject[] dots;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Initiliaze();    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void Initiliaze()
+    {
+        int dotToUse = Random.Range(0, dots.Length);
+        GameObject dot = Instantiate(dots[dotToUse], transform.position, Quaternion.identity);
+        dot.transform.parent = this.transform;
+        dot.name = this.gameObject.name;
+    }
+}
